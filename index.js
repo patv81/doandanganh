d4c3973126267b46c440ccd98b8f95eb2c6dc0bb
patv811 <glphamhoa@gmail.com>
2021-06-16 17:47:25 +0700

    delete key , useless comment

--- index.js
+++ index.js
@@ -85,7 +85,6 @@ function getInit(){
 
 var server = app.listen(port, function () {
     console.log("helloworld")
-    // let file = current.toLocaleDateString('vn-VN').replace(/\//g, '-')+'.txt';
     getInit();
 })
 
@@ -94,24 +93,35 @@ app.on('listening', function () {
 });
 
 const io = require('socket.io')(server);
-app.get('/test', function (req, res) {
-    res.render('user/test');
-})
+
 app.get('/', function (req, res) {
     getInit();
     res.render('user/dashboard',{"init":init})
 })
-app.get('/t2', function (req, res) {
-    res.render('user/t2')
-})
 app.get('/control', function (req, res) {
     res.render('user/control')
+})
+app.get('/log/temp-humid', function (req, res) {
+    testFolder = path.join(__dirname, 'static', 'file', 'temp-humid');
+
+    let files = fs.readdir(testFolder, (err, files) => {
+        console.log(files)
+        res.render('user/log', { files: files });
+    });
+})
+app.get('/log/drv', function (req, res) {
+    testFolder = path.join(__dirname, 'static', 'file', 'drv');
+
+    let files = fs.readdir(testFolder, (err, files) => {
+        console.log(files)
+        res.render('user/logdrv', { files: files });
+    });
 })
 const client = mqtt.connect("mqtt://io.adafruit.com", {
     protocolVersion: 3,
     clientId: 'my-device',
     username: 'ligemos',
-    password: 'aio_REnO87Vo6ropsnPNUUZRhMIgRaK0'
+    password: ''
 })
 // http://dadn.esp32thanhdanh.link/
 // const client = mqtt.connect("mqtt://io.adafruit.com", {
@@ -209,155 +219,6 @@ client.on('reconnect', function () {
     console.log("reconnect");
 });
 
-
-// ////////////////////////////////////////////////////////
-
-// const express = require("express");
-// const app = express();
-// const fs = require('fs');
-// const path = require('path');
-// const axios = require('axios');
-// require('dotenv').config();
-
-// let port = 3000;
-// let KEY = '';
-
-// app.use("/static", express.static(__dirname + "/static"));
-// app.set("view engine", "ejs");
-
-// const mqtt = require('mqtt');
-// const current = new Date()
-
-
-// var server = app.listen(port, function () {
-//     console.log("helloworld")
-// })
-// const io = require('socket.io')(server);
-// app.get('/test', function (req, res) {
-//     res.render('user/test');
-// })
-// app.get('/', function (req, res) {
-//     res.render('user/dashboard')
-// })
-// app.get('/t2', function (req, res) {
-//     res.render('user/t2')
-// })
-app.get('/log/temp-humid', function (req, res) {
-    testFolder = path.join(__dirname, 'static', 'file', 'temp-humid');
-
-    let files = fs.readdir(testFolder, (err, files) => {
-        console.log(files)
-        res.render('user/log', { files: files });
-    });
-})
-app.get('/log/drv', function (req, res) {
-    testFolder = path.join(__dirname, 'static', 'file', 'drv');
-
-    let files = fs.readdir(testFolder, (err, files) => {
-        console.log(files)
-        res.render('user/logdrv', { files: files });
-    });
-})
-// // const client = mqtt.connect("mqtt://io.adafruit.com", {
-// //     protocolVersion: 3,
-// //     clientId: 'my-device',
-// //     username: 'ligemos',
-// //     password: 'aio_bDjB19gteGG0sy3jNrCtbnRKZruA'
-// // })
-// // http://dadn.esp32thanhdanh.link/
-// let client = mqtt.connect("mqtt://io.adafruit.com", {
-//     protocolVersion: 3,
-//     clientId: 'Pham-Hoa',
-//     username: 'CSE_BBC',
-//     password: 'aio_YWqQ75LLnzE66cGrbMWNhCka1Xhb'
-//     // password:KEY,
-// })
-// const client2 = mqtt.connect("mqtt://io.adafruit.com", {
-//     protocolVersion: 3,
-//     clientId: 'Pham-Hoa',
-//     username: 'CSE_BBC1',
-//     password: 'aio_uUDl88Sq5jw4MFWMByHYXX3jvlUm'
-// })
-
-
-// io.on('connection', function (socket) {
-//     console.log("make socket connection", socket.id)
-//     socket.on("info-drv", formData => {
-//         // socket.broadcast.emit('infos', formDate);
-//         console.log("inside conection io.on info-drv", formData);
-//         data = {
-//             "id": "10",
-//             "name": "DRV_PWM",
-//             "data": formData,
-//             "unit": ""
-//         }
-//         console.log('socket.on(info-drv)', data);
-
-//         client.publish('CSE_BBC/feeds/bk-iot-drv', JSON.stringify(data), { qos: 2, retain: true }, function (err) {
-//             if (err) {
-//                 console.log("error publish", err);
-//             }else{
-//                 console.log("oublish drv")
-//             }
-//         });
-//     })
-// })
-
-
-
-
-// client.subscribe('CSE_BBC/feeds/bk-iot-temp-humid', function (err) {
-//     if (err) {
-//         console.log(err)
-//     }
-// })
-// client.subscribe('CSE_BBC/feeds/bk-iot-drv', function (err) {
-//     if (err) {
-//         console.log(err)
-//     }
-// })
-
-// client.on('connect', function () {
-// })
-
-// client.on('message', function (topic, message, packet) {
-//     // message is Buffer
-//     let data = message.toString();
-//     if (topic == "CSE_BBC/feeds/bk-iot-temp-humid") {
-
-//         let time = new Date().toLocaleString("en-US", { timeZone: "Asia/Ho_Chi_Minh" });
-//         io.emit('info-temp-humid', { data: data, time: time });
-
-//         let url = path.join(__dirname, 'static', 'file', 'temp-humid', current.toLocaleDateString('vn-VN').replace(/\//g, '-') + '.txt');
-//         data = JSON.parse(data);
-//         data.time = time
-//         fs.appendFile(url, JSON.stringify(data) + '\n', function (err) {
-//             if (err) throw err;
-//             console.log('Saved!');
-//         });
-//     }
-//     else if (topic == "CSE_BBC/feeds/bk-iot-drv") {
-//         console.log("data", data);
-//         let time = new Date().toLocaleString("en-US", { timeZone: "Asia/Ho_Chi_Minh" });
-//         io.emit('front-end-info-drv', { data: data, time: time });
-//         let url = path.join(__dirname, 'static', 'file', 'drv', current.toLocaleDateString('vn-VN').replace(/\//g, '-') + '.txt');
-//         data = JSON.parse(data);
-//         data.time = time
-//         fs.appendFile(url, JSON.stringify(data) + '\n', function (err) {
-//             if (err) throw err;
-//             console.log('Saved drv! :' ,'retain ', packet.retain );
-//         });
-//     }
-
-// })
-
-// client.on("error", function (error) {
-//     console.log("ERROR: ", error);
-// });
-
-// client.on('offline', function () {
-//     console.log("offline");
-// });
 
 // client.on('reconnect', function () {
 //     console.log("reconnect");
